a24253c764e0cc2c433724e96ae374a6f64ab328
金澤圭人 <keitodayo.@0107.icloud.com>
2024-05-14 10:44:59 +0900

    3.1をやりました。

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -2,18 +2,10 @@
 public class Start {
 
 	public static void main(String[] args) {
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	
 		
-		
-		MyFrame frame1=new MyFrame();
-		frame1.setLocation(200,100);
-		frame1.setSize(150,150);
-		MyFrame frame2=new MyFrame();
-		frame2.setLocation(350,200);
-		MyFrame frame3=new RactFrame();
-		frame3.setLocation(50,300);
-		MyFrame frame4=new RactFrame();
-		frame3.setLocation(50,300);
 		
 	
 
