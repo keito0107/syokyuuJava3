001bf321cf31dcc42b7d254b2d2eda4af5780f67
金澤圭人 <keitodayo.@0107.icloud.com>
2024-05-20 09:49:11 +0900

    3の6

--- 初級Java2.5/src/OvalTest.java
+++ 初級Java2.5/src/OvalTest.java
@@ -1,9 +1,11 @@
 
 public class OvalTest extends MyFrame{
 	public void run() {
-		fillOval(30,40,50,100);
-		fillOval(130,40,50,100);
-		fillRect(30,200,150,10);
+		fillRect(100,100,40,20);
+		fillRect(80,120,80,20);
+		fillOval(90,130,20,20);
+		fillOval(130,130,20,20);
+		
 	}
 	
 
